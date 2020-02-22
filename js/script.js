a324531c1cdadb822766d00178e0867a6c603c04
george kamau <flexkamau646@gmail.com>
2020-02-22 13:07:15 +0300

    added a a hover to images

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$(".brand img").click(function(){
+$("#jumbo p").toggle();
+});
+$(".dev img").click(function(){
+$("#tron p").toggle();
+});
+$(".product img").click(function(){
+$("#fluid p").toggle();
+});
+
+});
